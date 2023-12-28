d55287062acd4c5ed4a7b4d73da297e8d9be71b7
Nick G <nicholasgalante1997@gmail.com>
2023-12-27 22:43:12 -0700

    feat: Updates color palette, adds spacing tokens

---
+++ build/js/esm/index.mjs
@@ -0,0 +1,35 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 28 Dec 2023 05:42:06 GMT
+ */
+
+export const ColorBaseBlackOil = "#0c0c0c";
+export const ColorBaseBlackObsidian = "#0b1215";
+export const ColorBaseBlackSlate = "#0d1717";
+export const ColorBaseBlackMidnight = "#101720";
+export const ColorBaseBlackCharcoal = "#171717";
+export const ColorBaseBlackDeepNavy = "#011222";
+export const ColorBaseBlackRichBlack = "#020d19";
+export const ColorBaseWhite = "#ffffff";
+export const ColorBasePurpleDarkSoft100 = "#f189ff";
+export const ColorBasePurpleDarkSoft200 = "#ee6bff";
+export const ColorBasePurpleDarkSoft300 = "#ea46ff";
+export const ColorBaseGreenDarkSoft100 = "#69dc9e";
+export const ColorBaseBlueDarkSoft100 = "#c6e0ff";
+export const ColorBaseBlueLightSoft100 = "#3d405b";
+export const ColorBaseBlueLightSoft200 = "#2b2d42";
+export const ColorBaseRedLightSoft100 = "#e63946";
+export const ColorBaseRedLightSoft200 = "#d62828";
+export const ColorFontDarkHighContrast = "#ffffff";
+export const ColorFontDarkMediumContrast = "#ffffff";
+export const ColorFontDarkLowContrast = "#ffffff";
+export const ColorFontLightHighContrast = "#0c0c0c";
+export const ColorFontLightMediumContrast = "#0c0c0c";
+export const ColorFontLightLowContrast = "#0c0c0c";
+export const SizeSpacingNone = "0rem";
+export const SizeSpacingSmall100 = "0.125rem";
+export const SizeSpacingSmall200 = "0.25rem";
+export const SizeSpacingMedium100 = "0.5rem";
+export const SizeSpacingMedium200 = "0.75rem";
+export const SizeSpacingLarge100 = "1rem";
+export const SizeSpacingLarge200 = "1.5rem";
